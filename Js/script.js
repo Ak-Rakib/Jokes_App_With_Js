e0454a3e1a8ea357d38f20cfd3df59c58eb33582
Ak-Rakib <rkrakibsaimon@gmail.com>
2024-05-03 23:42:28 +0600

    write basic code html and js

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+const jokeText = document.querySelector('.text_of_joke');
+const nextJokeBtn = document.querySelector('.next_btn');
+const shareBtn = document.querySelector('.share_btn');
+
+nextJokeBtn.addEventListener('click', function(){
+    alert('testing code');
+});
